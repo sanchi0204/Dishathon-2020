b8feece961350859c3a607e0a482ce57181e3d3d
amitabhsharma <amitabhs30@gmail.com>
2020-06-20 20:34:01 +0530

    Correction done in Database for Reviews

--- app/src/main/java/com/example/watcho/ShowingReviews.java
+++ app/src/main/java/com/example/watcho/ShowingReviews.java
@@ -1,19 +1,23 @@
 package com.example.watcho;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.watcho.Adapters.ReviewAdapter;
 import com.example.watcho.Model.Review;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -26,6 +30,7 @@ public class ShowingReviews extends AppCompatActivity {
     ReviewAdapter reviewAdapter;
     List<Review> reviewList;
     FirebaseFirestore db= FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference=db.collection("Reviews");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +40,46 @@ public class ShowingReviews extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         reviewList=new ArrayList<>();
         final Review review=new Review();
-        db.collection("Reviews")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful())
-                        {
-                            for(QueryDocumentSnapshot documentSnapshot : Objects.requireNonNull(task.getResult())){
-                                review.setName(documentSnapshot.get("name").toString());
-                                review.setReview(documentSnapshot.get("review").toString());
-                                review.setRating(documentSnapshot.get("rating").toString());
-                                review.setTime(documentSnapshot.get("time").toString());
-                                reviewList.add(review);
-                            }
-                            reviewAdapter=new ReviewAdapter(ShowingReviews.this,reviewList);
-                            recyclerView.setAdapter(reviewAdapter);
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+        collectionReference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
             @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ShowingReviews.this, e.toString(), Toast.LENGTH_SHORT).show();
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if(e!=null)
+                {
+                    return;
+                }
+                else
+                {
+                    for(QueryDocumentSnapshot documentSnapshot:queryDocumentSnapshots)
+                    {
+                        Review review=documentSnapshot.toObject(Review.class);
+                        reviewList.add(review);
+                    }
+                    reviewAdapter=new ReviewAdapter(ShowingReviews.this,reviewList);
+                    recyclerView.setAdapter(reviewAdapter);
+                }
             }
         });
+//        db.collection("Reviews")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if(task.isSuccessful())
+//                        {
+//                            for(QueryDocumentSnapshot documentSnapshot : Objects.requireNonNull(task.getResult())){
+//
+//                            }
+//                            reviewAdapter=new ReviewAdapter(ShowingReviews.this,reviewList);
+//                            recyclerView.setAdapter(reviewAdapter);
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(ShowingReviews.this, e.toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 }
